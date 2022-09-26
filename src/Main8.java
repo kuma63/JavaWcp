7885fc875a5c3ab1242aa7ad1e7da990c9c97d7a
kuma63 <104369478+kuma63@users.noreply.github.com>
2022-09-26 17:59:35 +0900

    [start]java8-3

---
+++ src/Main8.java
@@ -0,0 +1,10 @@
+
+public class Main8 {
+	public static void main(String[] args) {
+        //	勇者よ、この仮想世界に生まれよ！
+        //	お化けキノコよ、この仮想世界に生まれよ！
+        //	勇者よ、戦え！
+        //	お化けキノコよ、逃げろ！
+	}
+
+}
