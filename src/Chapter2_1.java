647ffaf008eaaf88bcee225666a33cfe8c9abd79
kuma63 <104369478+kuma63@users.noreply.github.com>
2022-10-07 19:57:25 +0900

    [finish]java2-1

---
+++ src/Chapter2_1.java
@@ -0,0 +1,9 @@
+
+public class Chapter2_1 {
+	public static void main(String[] args) {
+		Human2_1 human = new Human2_1();
+		Human2_1.HumanLeg leg = human.new HumanLeg();
+		leg.kick();
+	}
+
+}
---
+++ src/Human2_1.java
@@ -0,0 +1,9 @@
+
+public class Human2_1 {
+	public class HumanLeg{
+		public void kick() {
+			System.out.println("蹴る");
+		}
+	}
+
+}
