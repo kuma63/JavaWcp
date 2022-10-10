c32dc41659ae554b3e88194193898194ddb78e01
kuma63 <104369478+kuma63@users.noreply.github.com>
2022-10-10 19:57:06 +0900

    [finish]java-8

--- src/Main8.java
+++ src/Main8.java
@@ -2,9 +2,23 @@
 public class Main8 {
 	public static void main(String[] args) {
         //	勇者よ、この仮想世界に生まれよ！
-        //	お化けキノコよ、この仮想世界に生まれよ！
-        //	勇者よ、戦え！
-        //	お化けキノコよ、逃げろ！
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+
+		h.slip();
+		m1.run();
+		m2.run();
+		h.run();
+
 	}
 
 }
